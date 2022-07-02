9e66f28faa11f50c3f45a73f1530815d40857f84
Olotu Adah <adaholotu@gmail.com>
2022-07-02 16:25:30 +0100

    stripe payment: checkout session on the server side

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { authenticate } = require("../controllers/authenticationController");
+const { getCheckoutSession } = require("../controllers/bookingController");
+
+const bookingRouter = express.Router();
+
+bookingRouter.use(authenticate);
+
+//tourId is the id of the tour being booked
+bookingRouter.get("/checkout-session/:tourId", getCheckoutSession);
+
+module.exports = bookingRouter;
